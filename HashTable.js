ac686efb6f15190cb50ad15b5f31af15e3b95e4b
Shay Kelley <Shay@macbook-pro-6.lan>
2020-07-30 08:52:21 -0400

    added starter code constructor and hash method

--- HashTable.js
+++ HashTable.js
@@ -1,27 +1,34 @@
 class HashTable {
-  constructor(size) {
+  constructor(size = 11) {
     // initialize table size - prime number size is recommended to avoid clustering
     // intialize the table to have "size" number of elements, set to null
     // the table will be an array named "table"
+    this.table = new Array(size)
   }
 
   hash(key) {
     // calculate and return an integer value based key, like in the lesson
     // remember, if you are using modulus, it is recommended to use a prime number to avoid clustering
+    let num = 7
+    for (let i = 0; i < key.length; i += 1) {
+      num += key.charCodeAt(i)
+    }
+    let index = num % this.table.length
+    return index
   }
 
   insert(key, value) {
     // hash the key to get an integer index
 
     // if there's no array at that index in the table 
-      // create one and add it
-      // and insert this key value pair into the new array
+    // create one and add it
+    // and insert this key value pair into the new array
 
     // if there's a array at that index
-      // if a key value pair already exists with the same key, update the data in that key value pair to store the new value
-    
+    // if a key value pair already exists with the same key, update the data in that key value pair to store the new value
+
     // otherwise
-      // add a new key value pair with the given value to the end of the array
+    // add a new key value pair with the given value to the end of the array
 
     // for the convenience of the user, you might wish to return the key value pair, or you can just return true
   }
@@ -32,7 +39,7 @@ class HashTable {
     // if the key is found, return the value
     // if not, return -1
   }
-  
+
   delete(key) {
     // lookup the key (i.e. hash it to get an index)
     // if the key is, in fact, in the array, delete that key value pair and return it
